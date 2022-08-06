eb32543ad9a0e67b5c49cab7d771f9f64f878393
Chris Athanas <realityexpander@gmail.com>
2022-08-06 02:38:28 -0500

    Add comments

--- src/main/kotlin/com/realityexpander/Application.kt
+++ src/main/kotlin/com/realityexpander/Application.kt
@@ -10,6 +10,16 @@ import io.ktor.client.features.json.serializer.*
 
 // Make an account on OneSignal and get your app ID and Rest API key
 
+// test this server from browser:
+// http://192.168.0.186:8083/sendNotification?title=123&description=456
+
+// Dashboards:
+// https://console.firebase.google.com/u/0/project/ktorpushnotifications/settings/cloudmessaging
+// https://console.cloud.google.com/apis/api/googlecloudmessaging.googleapis.com/quotas?project=ktorpushnotifications&authuser=0&hl=en
+// https://app.onesignal.com/apps/01206360-f412-41b8-b4ed-830f026b4660/settings/configure/google-android
+
+// https://documentation.onesignal.com/reference/create-notification
+
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
